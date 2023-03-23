c83191615bec12f260856a5d11db1dec2782e2c7
Van Phat <vanphat15it@gmail.com>
2023-03-23 15:43:41 +0700

    fix bai 6 chuong 3

--- Chuong 3/bai6chuong3.cpp
+++ Chuong 3/bai6chuong3.cpp
@@ -2,12 +2,12 @@
 
 using namespace std;
 
-double sumFactorial(long long n, long long x){
-	double rs = 1.0;
-    double gt = 1.0;
+long double sumFactorial(long long n, long long x){
+	long double rs = 1.0;
+    long double gt = 1.0;
 	for(long long i = 1; i <= n; i++){
-		gt *= (double)x/i;
-		rs += gt;
+		gt = gt * (long double)x/i;
+		rs = rs + gt;
 	}
 	return rs;
 }
@@ -15,5 +15,5 @@ double sumFactorial(long long n, long long x){
 int main(){
 	long long n,x;
 	cin >> n >> x;
-	cout << setprecision(2) << (double)sumFactorial(n,x);
+	cout << setprecision(2) << fixed << (long double)sumFactorial(n,x);
 }
